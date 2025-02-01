cf0aaf8792b631faa47d627907ca05a3be9dc14f
moose96 <piotr.losiak96@gmail.com>
2025-02-02 00:28:49 +0100

    chore: add base property for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/canvas-editor/',
   plugins: [react(), tailwindcss(), svgr({ svgrOptions: { titleProp: true, svgProps: { className: 'icon' } } })],
 });
